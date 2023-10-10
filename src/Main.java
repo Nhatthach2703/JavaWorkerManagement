39c0111f5d672d6d5ba99043a28e6ace34dbe481
Nhatthach2703 <61878027+Nhatthach2703@users.noreply.github.com>
2023-10-10 14:21:10 +0700

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,6 @@ import Controller.WorkerManager;
  */
 public class Main {
     public static void main(String[] args) {
-        new WorkerManager().run();
+        new WorkerManager().run();     
     }
 }
